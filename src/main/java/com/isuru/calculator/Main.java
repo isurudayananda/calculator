f79e10c28542db202ac44fb1d03443fa0527a0c8
isurudayananda <129027427+isurudayananda@users.noreply.github.com>
2023-09-17 23:07:42 +0530

    Update Main.java

--- src/main/java/com/isuru/calculator/Main.java
+++ src/main/java/com/isuru/calculator/Main.java
@@ -1,7 +1,43 @@
 package com.isuru.calculator;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello, World!");
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        double num1=sc.nextDouble();
+        double num2=sc.nextDouble();
+
+        System.out.println("Enter the operator,+,-,*,/");
+        char opr=sc.next().charAt(0);
+
+        double result=0;
+
+
+        switch (opr){
+            case '+':
+                result=num1+num2;
+                break;
+            case '-':
+                result=num1-num2;
+                break;
+            case '*':
+                result=num1*num2;
+                break;
+            case '/':
+                result=num1/num2;
+                break;
+            default:
+                System.out.println("invalid input");
+        }
+
+        System.out.println(num1 +" "+opr+num2+" "+"= "+result);
+
+
+
+
+
+
+
     }
 }
